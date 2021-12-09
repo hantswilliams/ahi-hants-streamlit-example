8906f4cd396748e5afe6ed309c537193bc5b3847
Hants Williams <hantsawilliams@gmail.com>
2021-12-08 20:35:16 -0500

    created initial file
    for streamlit

---
+++ streamlit_example.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  8 20:33:28 2021
+
+@author: hantswilliams
+"""
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.title('Uber pickups in NYC')
