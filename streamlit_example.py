9500bdc805e8308b16a838caca8bdf51d0ddc2d6
Hants Williams <hantsawilliams@gmail.com>
2021-12-08 20:55:24 -0500

    Update streamlit_example.py

--- streamlit_example.py
+++ streamlit_example.py
@@ -14,7 +14,16 @@ import numpy as np
 
 st.title('CMS - Hospital Data ')
 
+st.text('For this assignment, we were tasked with doing X, Y, Z; For the first part, here are some transofrmation that were applied....")
+
+st.code("""
+        inpatient_ny.groupby('drg_definition')['total_discharges'].sum().reset_index()
+        """)
+
+
 df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/hospital_info.csv')
 
 
 st.dataframe(df)
+
+
